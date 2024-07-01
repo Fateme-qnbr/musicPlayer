318649f84c748c47d189c1642a26832f61523de1
Fateme-qnbr <fatemeqanbari010@gmail.com>
2024-07-01 11:30:45 +0330

    Artist(img) first check

--- musicPlayer/src/Data/ArtistsData.js
+++ musicPlayer/src/Data/ArtistsData.js
@@ -7,118 +7,76 @@ export const Data=[
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'2',
         name:'On My Way',
         singer:'Selina Gomez',
         image:'2.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },{
-        id:'1',
+        id:'3',
         name:'On My Way',
         singer:'Alan Walker',
         image:'3.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },{
-        id:'1',
+        id:'4',
         name:'On My Way',
         singer:'Alan Walker',
         image:'4.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },{
-        id:'1',
+        id:'5',
         name:'On My Way',
         singer:'Alan Walker',
         image:'5.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },{
-        id:'1',
+        id:'6',
         name:'On My Way',
         singer:'Alan Walker',
         image:'6.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'7',
         name:'On My Way',
         singer:'Alan Walker',
         image:'7.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'8',
         name:'On My Way',
         singer:'Alan Walker',
         image:'8.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'9',
         name:'On My Way',
-        singer:'Alan Walker',
+        singer:'Ariana Grande',
         image:'9.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'10',
         name:'On My Way',
-        singer:'Alan Walker',
+        singer:'Taylor Swift',
         image:'10.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'11.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
+        id:'12',
         name:'On My Way',
         singer:'Alan Walker',
         image:'12.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
     {
-        id:'1',
+        id:'13',
         name:'On My Way',
         singer:'Alan Walker',
         image:'13.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'14.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'15.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'16.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'17.jpg',
-        desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
-    },
-    {
-        id:'1',
-        name:'On My Way',
-        singer:'Alan Walker',
-        image:'18.jpg',
         desc: 'Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Placerat duis ultricies lacus sed turpis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Scelerisque in dictum non consectetur a erat.'
     },
 ]
